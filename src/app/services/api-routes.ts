5be39385d7090be40109fcd6f26299b865ec08eb
stefan466 <smaksimovic3519rn@raf.rs>
2024-05-14 18:38:00 +0200

    Merge branch 'dev' of https://github.com/RAF-SI-2023/Banka-2-Frontend into dev

--- src/app/services/api-routes.ts
+++ src/app/services/api-routes.ts
@@ -15,6 +15,8 @@ export const ApiRoutes = {
 		passwordChange: '/users/password-change',
 		createEmployee: '/users/create/employee',
 		createAgent: '/users/create/agent',
+		resetAgentsLeftLimit: '/users/agent-limit/reset',
+		getAgentsLeftLimit: '/users/agent-limit',
 		createPrivateClient: '/users/public/create/private-client',
 		createCorporateClient: '/users/public/create/corporate-client',
 		updateEmployee: '/users/update/employee',
